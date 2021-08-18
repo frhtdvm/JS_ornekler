8abb9d7b59a262c5c2e585f112ebd8723beea6e8
blaze187 <frhtdvm@gmail.com>
2021-08-18 16:54:54 +0200

    alistirmalar bitti html de

---
+++ js/prompt.js
@@ -0,0 +1,8 @@
+let title = document.getElementById("baslik");
+title.innerHTML = "Degisen Bilgi";
+console.log(title.innerHTML);
+
+let link = document.querySelector("#linkbilgisi");
+link.innerHTML = "Degistirilmis yeni Link"
+link.style.color = "green"
+console.log(linkbilgisi.innerHTML);
