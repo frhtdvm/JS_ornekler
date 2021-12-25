24d13418cf2f7ff8c6ed0c5ee20bc2c17b2038e8
blaze187 <frhtdvm@gmail.com>
2021-12-25 21:02:49 +0100

    form örnegi

--- JS-Tekrar/tekrarPart4.js
+++ JS-Tekrar/tekrarPart4.js
@@ -1,2 +1,29 @@
 //* 25 Local Storage veri ekleme, silme, duzenleme
+//! Local Storage bu konuzu daha sonra tekrarlanmali
 
+//* 27 Form ve Form Submit yöntemi
+/* let formDOM = document.querySelector("#userForm");
+formDOM.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault(); 
+  console.log("Islem gerceklesti!");
+} */
+
+//* 28 input icerisinde deger almak 
+/* let formDOM = document.querySelector("#userForm");
+formDOM.addEventListener('submit', formSubmit); 
+
+function formSubmit(event) {
+  event.preventDefault(); 
+  console.log("Islem gerceklesti!");
+} */
+//* 29 Formlar ile ilgili Egzersiz
+let userFormDOM = document.querySelector("#userForm");
+userFormDOM.addeventListener('submit', formHandler);
+
+function formHandler(event){
+  event.preventDefault()
+  const USERN_NAME = document.querySelector("#username");
+  const SCORE = document.querySelector("#score")
+}
