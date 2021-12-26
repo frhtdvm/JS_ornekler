9e447e3bb122fcda97cbdbb540b622ca33019a7f
blaze187 <frhtdvm@gmail.com>
2021-12-26 11:26:49 +0100

    form ornegi yapildi ve birsey anlasilmadi

--- JS-Tekrar/tekrarPart4.js
+++ JS-Tekrar/tekrarPart4.js
@@ -10,7 +10,7 @@ function formSubmit(event) {
   console.log("Islem gerceklesti!");
 } */
 
-//* 28 input icerisinde deger almak 
+//* 28 input icerisinde deger almak
 /* let formDOM = document.querySelector("#userForm");
 formDOM.addEventListener('submit', formSubmit); 
 
@@ -20,10 +20,40 @@ function formSubmit(event) {
 } */
 //* 29 Formlar ile ilgili Egzersiz
 let userFormDOM = document.querySelector("#userForm");
-userFormDOM.addeventListener('submit', formHandler);
+userFormDOM.addEventListener("submit", formHandler);
+const alertDOM = document.querySelector("#alert");
 
-function formHandler(event){
-  event.preventDefault()
-  const USERN_NAME = document.querySelector("#username");
-  const SCORE = document.querySelector("#score")
+const alertFunction = (title, message) => `
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>${title}</strong> ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`;
+
+function formHandler(event) {
+  event.preventDefault();
+  const USER_NAME = document.querySelector("#username");
+  const SCORE = document.querySelector("#score");
+
+  if (USER_NAME.value && SCORE.value) {
+    addItem(USER_NAME.value, SCORE.value); // gonderdikten sonra sifirladik
+    USER_NAME.value = "";
+    SCORE.value = "";
+  } else {
+    alertDOM.innerHTML = alertFunction("Baslik bilgisi","Hatali Eksik Bilgi girildi");
+  }
 }
+
+let userListDOM = document.querySelector("#userList");
+const addItem = (userName, score) => {
+  let liDOM = document.createElement("li");
+  liDOM.innerHTML = `${userName} 
+    <span class="badge bg-primary rounded-pill">${score}</span> `;
+  liDOM.classList.add(
+    "list-group-item",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+  );
+  userListDOM.append(liDOM);
+};
